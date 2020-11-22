74ce5e1bc17e05f315344e13ae31421e4241558d
yesmine <yesminerekik5@gmail.com>
2020-11-22 16:44:03 +0100

    MRT-3:check R key values

--- minirt/src/Check.c
+++ minirt/src/Check.c
@@ -13,6 +13,33 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
+int	Check_values_if_Rkey(t_config_map *element)
+{
+	int	i = 0;
+	char *first_num;
+	char *second_num;
+
+	if(strchr(element->key, 'R'))
+	{
+		first_num = ft_itoa(ft_atoi(element->value[0]));
+		second_num = ft_itoa(ft_atoi(element->value[1]));
+		if(get_num_of_non_null_value(element->value) != 2)
+		{
+			ft_putstr_fd("number of values for R key is not correct", 1);
+			return -1;
+		}
+		else if(ft_strncmp(first_num, element->value[0], ft_strlen(element->value[0])) == 0 &&
+				ft_strncmp(second_num, element->value[1], ft_strlen(element->value[1])) == 0 )
+			return 1;
+		else
+		{
+			ft_putstr_fd("number of values for R key is not correct", 1);
+			return -1;
+		}
+	}
+	return(0);
+}
+
 int Check_key(t_list *lines)
 {
 	t_list	*tmp;
@@ -31,6 +58,8 @@ int Check_key(t_list *lines)
 			else
 				i++;
 		}
+		if(Check_values_if_Rkey(element) == -1)
+			return -1;
 		if(i == sizeof(keys) / sizeof(char*))
 		{
 		//	perror("error");
@@ -43,3 +72,13 @@ int Check_key(t_list *lines)
 	}
 	return 1;
 }
+
+int	get_num_of_non_null_value(char *value[])
+{
+	int	i;
+	i = 0;
+	while(value[i] != NULL)
+		i++;
+	return i;
+}
+
